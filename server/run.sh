f3bdeb22d6f372b22ae4624334729d16862109e0
Coen de Vente <coendevente@gmail.com>
2025-03-19 09:52:01 +0100

    update

--- server/run.sh
+++ server/run.sh
@@ -1,1 +1,1 @@
-docker run -p 1527:1527 --gpus all --rm -d -v /tmp/sam2_images:/home/user/tmp/sam2_images nninteractive_slicer_server
+docker run -p 1527:1527 --gpus all --rm -d nninteractive_slicer_server
